c1e4490c76205040f70e9e6d7fe9fc8a68e9a43e
Anna Langner <annalangner06@gmail.com>
2019-06-24 09:47:40 +0200

    Layout implementation

--- client/App.js
+++ client/App.js
@@ -12,4 +12,33 @@ class App extends Component {
     render() {
         return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
     }
+
+    renderLayout() {
+        return (
+            <div className={styles.App}>
+                <div className={styles.AppHeader}>
+                    <div className={styles.AppTitle}>
+                        ChatApp
+                    </div>
+                    <div className={styles.AppRoom}>
+                        AppRoom
+                    </div>
+                </div>
+                <div className={styles.AppBody}>
+                    <UsersList
+                        users={this.state.users}
+                    />
+                    <div className={styles.MessageWrapper}>
+                        <MessageList
+                            messages={this.state.messages}
+                        />
+                        <MessageForm
+                            onMessageSubmit={message => this.handleMessageSubmit(message)}
+                            name={this.state.name}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
