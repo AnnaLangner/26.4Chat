a1f2d8f668a77fb2a08a98e4b208be3fb72b7764
Anna Langner <annalangner06@gmail.com>
2019-06-24 12:17:19 +0200

    implementation of the remaining logic of the App.js component

--- client/App.js
+++ client/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import io from 'socket.io-client';
 
+import styles from './App.css';
+
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import UsersList from './UsersList';
+import UserForm from './UserForm';
 const socket = io('/');
 
 class App extends Component {
@@ -9,6 +15,31 @@ class App extends Component {
         super(props);
         this.state = {users: [], messages: [], text: '', name: ''};
     }
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({users}) => this.chatUpdate(users));
+    }
+
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+    }
+
+    chatUpdate(users) {
+        this.setState({users});
+    }
+
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+        socket.emit('message', message);
+    }
+
+    handleUserSubmit(name) {
+        this.setState({name});
+        socket.emit('join', name);
+    }
+
     render() {
         return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
     }
@@ -40,5 +71,8 @@ class App extends Component {
                 </div>
             </div>
         );
+    }
+    renderUserForm() {
+        return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
     }
 }
