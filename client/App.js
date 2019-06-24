237ee897854d9781890bcc60d99badf85153fb13
Anna Langner <annalangner06@gmail.com>
2019-06-24 09:34:06 +0200

    Defining the initial state of the app

--- client/App.js
+++ client/App.js
@@ -3,3 +3,13 @@ import { hot } from 'react-hot-loader';
 import io from 'socket.io-client';
 
 const socket = io('/');
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {users: [], messages: [], text: '', name: ''};
+    }
+    render() {
+        return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
+    }
+}
